9513f4d47dbeb83040d5b1d23d438704cfd67c27
유니 <wvvw.uni@gmail.com>
2022-02-20 21:10:34 +0900

    220220_79 change color when click a button

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -2,18 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bottom_navigationbar/constants/matrial_white.dart';
-import 'package:flutter_bottom_navigationbar/constants/screen_size.dart';
 import 'package:flutter_bottom_navigationbar/widgets/rounded_avatar.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({Key? key}) : super(key: key);
+  SearchScreen({Key? key}) : super(key: key);
 
   @override
   _SearchScreenState createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<bool> followings = List.generate(30, (index) => false);
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -27,18 +26,31 @@ class _SearchScreenState extends State<SearchScreen> {
                   'userName $index',
                 ),
                 subtitle: Text('user bio number $index'),
-                trailing: Container(
-                  alignment: Alignment.center,
-                  height: 30,
-                  width: 80,
-                  decoration: BoxDecoration(
-                      color: Colors.indigoAccent[100],
-                      border:
-                          Border.all(color: Colors.indigoAccent, width: 0.5),
-                      borderRadius: BorderRadius.circular(9)),
-                  child: Text(
-                    'following',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                trailing: InkWell(
+                  onTap: () {
+                    setState(() {
+                      followings[index] = !followings[index];
+                    });
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 30,
+                    width: 80,
+                    decoration: BoxDecoration(
+                        color: followings[index]
+                            ? Colors.redAccent[100]
+                            : Colors.indigoAccent[100],
+                        border: Border.all(
+                            color: followings[index]
+                                ? Colors.redAccent
+                                : Colors.indigoAccent,
+                            width: 0.5),
+                        borderRadius: BorderRadius.circular(9)),
+                    child: Text(
+                      'following',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
                   ),
                 ),
               );
@@ -48,6 +60,6 @@ class _SearchScreenState extends State<SearchScreen> {
                 color: Colors.grey[30],
               );
             },
-            itemCount: 30));
+            itemCount: followings.length));
   }
 }
