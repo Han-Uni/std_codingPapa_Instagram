04490acc270cc4744dd6e8ae597e803c208fd77a
유니 <wvvw.uni@gmail.com>
2022-02-21 21:53:40 +0900

    220221_82 firebase auth 상태 모델 생성

---
+++ lib/models/firebase_auth_state.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+// firebase_auth: ^0.18.0부터는 변경 전에서 변경 후 값을 사용하세요!^^
+// 변경전 => 변경후
+// FirebaseUser => User
+// AuthResult => UserCredential
+// currentUser() => currentUser
+// onAuthStateChanged => authStateChanges()
+
+class FirebaseAuthState extends ChangeNotifier {
+  FirebaseAuthStatus _firebaseAuthStatus = FirebaseAuthStatus.progress;
+  User? _user;
+
+  void changeFirebaseAuthStatus(FirebaseAuthStatus firebaseAuthStatus) {
+    if (firebaseAuthStatus != null) {
+      _firebaseAuthStatus = firebaseAuthStatus;
+    } else {
+      if (_user != null) {
+        _firebaseAuthStatus = FirebaseAuthStatus.signin;
+      } else {
+        firebaseAuthStatus = FirebaseAuthStatus.signout;
+      }
+    }
+
+    notifyListeners();
+  }
+}
+
+enum FirebaseAuthStatus { signout, progress, signin }
