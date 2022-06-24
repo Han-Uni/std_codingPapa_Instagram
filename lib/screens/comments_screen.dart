6045980f19bf5bc9b384d55a00f715f3aadbe988
유니 <wvvw.uni@gmail.com>
2022-06-25 05:22:34 +0900

    220625_148 comments page input layout

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bottom_navigationbar/constants/common_size.dart';
 
 class CommentsScreen extends StatefulWidget {
   const CommentsScreen({Key? key}) : super(key: key);
@@ -8,8 +9,54 @@ class CommentsScreen extends StatefulWidget {
 }
 
 class _CommentsScreenState extends State<CommentsScreen> {
+  TextEditingController _textEditingController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.amber);
+    return Scaffold(
+      body: Form(
+          child: Column(
+        children: <Widget>[
+          Expanded(
+              child: Container(
+            color: Colors.amber,
+          )),
+          Row(
+            children: <Widget>[
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: common_gap),
+                child: TextFormField(
+                  controller: _textEditingController,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                      hintText: 'Add a commnet...', border: InputBorder.none),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Input Something';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+              )),
+              TextButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    //todo push a comments
+                  }
+                  ;
+                },
+                child: Text(
+                  'POST',
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
+          )
+        ],
+      )),
+    );
   }
 }
