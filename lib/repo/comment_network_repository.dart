eafb9bf170e88b8032a99c484cc618b8c791b65e
유니 <wvvw.uni@gmail.com>
2022-06-25 06:38:34 +0900

    220625_코멘트 맨 아래로 자동 스크롤하는 방법

--- lib/repo/comment_network_repository.dart
+++ lib/repo/comment_network_repository.dart
@@ -32,7 +32,7 @@ class CommentNetworkRepository with Transformers {
         .collection(COLLECTION_POSTS)
         .doc(postKey)
         .collection(COLLECTION_COMMENTS)
-        .orderBy(KEY_COMMENTTIME)
+        .orderBy(KEY_COMMENTTIME, descending: true)
         .snapshots()
         .transform(toComments);
   }
